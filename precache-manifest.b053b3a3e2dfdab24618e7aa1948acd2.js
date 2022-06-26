09455014709bf0a4b245e054e8df2dd4f6ca6361
Pratick Gupta <pratickgupta56@gmail.com>
2022-06-26 17:58:18 +0530

    Updates

---
+++ precache-manifest.b053b3a3e2dfdab24618e7aa1948acd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4546b549d0a0119de00",
+    "url": "/static/css/main.53d63857.chunk.css"
+  },
+  {
+    "revision": "e4546b549d0a0119de00",
+    "url": "/static/js/main.f6876c7c.chunk.js"
+  },
+  {
+    "revision": "5e16f48e3c311f27a098",
+    "url": "/static/js/runtime~main.2fe9e7f1.js"
+  },
+  {
+    "revision": "22126cfa7e659211b9ee",
+    "url": "/static/css/2.c009acd3.chunk.css"
+  },
+  {
+    "revision": "22126cfa7e659211b9ee",
+    "url": "/static/js/2.02448105.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "4733f94bc61e8debd387da2460ead4ac",
+    "url": "/index.html"
+  }
+];
